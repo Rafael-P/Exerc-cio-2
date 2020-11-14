9ddf92c9c89537fc24439319aaa3bf0bbb0f7830
Rafael-P <r4f4el_porto@hotmail.com>
2020-11-14 20:30:59 -0300

    exercício 2 adicionado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Digite seu ano de nascimento");
+            int nascimento = int.Parse(Console.ReadLine());
+
+            int idade = 2020-nascimento;
+            Console.WriteLine("Sua idade é "+idade);
+
+            int semanas = idade*52;
+            Console.WriteLine("Sua idade em semanas é "+semanas);
+            
+        }//FIM DO MAIN
+    }
+}
